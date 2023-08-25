63a91617c509937055066132f3e0d56caa8326df
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-08-25 10:56:02 -0300

    Projet finally

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from Email.email_sender import Emailer
+from App.one_destination import _main
 
-email = Emailer('jhonatandevaprender1@gmail.com', 'gxnxkxrvwnwbobbs')
 
-lista_contatos = ['jhonatands.dev.2030@gmail.com',
-                  'jhonatandevaprender1@gmail.com']
-mensagem = '''
-Olá seu pacote acaba de chegar nos correios!
-'''
-email.definir_conteudo(topico='Seu pacote chegou!', email_remetente='jhonatands.dev.2030@gmail.com',
-                       lista_contatos=lista_contatos, conteudo_email=mensagem)
+if __name__ == '__main__':
+    _main()
 
-imagens = ['bluesky.jpg', 'retro.jpg']
-email.anexar_imagem(imagens)
-
-arquivos = ['csv_exemplo.csv', 'exemplo_word.docx',
-            'ExemploPlanilha.xlsx', 'PDF_Exemplo.pdf', 'Untitled presentation.pptx']
-email.anexar_arquivos(arquivos)
-email.enviar_email(intervalo_em_segundos=30)
